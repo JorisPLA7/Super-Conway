c0633cc92de689002dec2d2b5ac230073f31b75f
tuks <joris@placette.fr>
2017-05-25 22:52:50 +0200

    pitite grenouille :frog:  & paradigme objet

--- tests/listes.py
+++ tests/listes.py
@@ -1,50 +1,47 @@
-xLen = 6
-yLen = 6
+class Draw():
+    def __init__(self,xLen,yLen):
+        self.xLen = 6
+        self.yLen = 6
+        self.draw = []
+        self.startBaton()
 
-
-
-global draw
-draw = []
-def reverse(val):
-    if val == 1 : return 0
-    if val == 0 : return 1
-    else : return '?'
-def refresh(x,y,i):
-    S = 0
-    for nearx in range(0,3):
-        for neary in range(0,3):
-            if not nearx == neary == 1:
-                try:
-                    S += draw[i][x-1+nearx][y-1+neary]
-                except:
-                    pass
-    
-
-    rep = 0
-    if draw[i][x][y]:
-        if S == 2:
+    def __refresh(self,x,y,i):
+        S = 0
+        for nearx in range(0,3):
+            for neary in range(0,3):
+                if not nearx == neary == 1:
+                    try:
+                        S += self.draw[i][x-1+nearx][y-1+neary]
+                    except:
+                        pass #si par exemple on est proche d'un bord, on évite le plantage lors de la lecture dans un index bidon
+        rep = 0
+        if self.draw[i][x][y]:
+            if S == 2:
+                rep = 1
+        elif S == 3:
             rep = 1
-    elif S == 3:
-        rep = 1
-    if S <2:
-        rep = 0
-    if S >3:
-        rep = 0
-    return rep
+        if S <2:
+            rep = 0
+        if S >3:
+            rep = 0
+        return rep
 
-    #return reverse(draw1[x][y])
+    def startBaton(self):
+        self.draw.append([[0, 1, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 1, 1, 1], [0, 0, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0]])
+        #self.draw1 = [ [0 for y in range(self.yLen)] for x in range(self.xLen)]
+        for n in range(0,self.xLen):
+            print(self.draw[0][n])
+        print("")
+        self.i = 0
+
+    def increment(self, increment):
+        for i in range(self.i, self.i+ increment):
+            self.draw.append([ [self.__refresh(x,y,self.i-1) for y in range(self.yLen)] for x in range(self.xLen)])
+            for n in range(0,self.xLen):
+                print(self.draw[i][n])
+            print("")
 
 
 
-draw.append([[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]])
-#draw1 = [ [0 for y in range(yLen)] for x in range(xLen)]
-for n in range(0,xLen):
-    print(draw[0][n])
-print("")
-steps = 3
-#steps = input("Nombre d'itérations: ")
-for i in range(1,int(steps)+1):
-    draw.append([ [refresh(x,y,i-1) for y in range(yLen)] for x in range(xLen)])
-    for n in range(0,xLen):
-        print(draw[i][n])
-    print("")
+MyDraw = Draw(6,6)
+MyDraw.increment(10)
