8acc9e4da9f9482ef359ece90e85300a3949c98d
tuks <joris@placette.fr>
2017-06-03 19:10:08 +0200

    rm lib in gitignore

---
+++ lib/web.py
@@ -0,0 +1,12 @@
+helppage = "https://github.com/JorisPLA7/Super-Conway/blob/master/README.md"
+githubpage = "https://github.com/JorisPLA7/Super-Conway/"
+
+import webbrowser
+
+def help():
+    webbrowser.open(helppage)
+def github():
+    webbrowser.open(githubpage)
+
+if __name__ == '__main__':
+    help()
